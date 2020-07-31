a2e2a358ff1345e441a6c97fd4266354a4f08d9b
John McNay <john.w.mcnay@gmail.com>
2020-07-31 12:26:47 -0500

    completed iterating.js Arrays exercise

--- js/iterating.js
+++ js/iterating.js
@@ -6,30 +6,40 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-    var names = ["Mayra", "Anna", "Adriana", "Karen"];
+    var names = ["Mayra", "Ana", "Adriana", "Karen"];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-    console.log(names.length)
+    console.log(names.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+    console.log("==Direct Access==");
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    console.log("==For Loop==");
+    for (var i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+    console.log("==For Each==");
+    names.forEach( name => {
+        console.log(name);
+    })
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +53,20 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+    const first = arr => {
+        return arr[0];
+    };
+
+    const second = arr => {
+        return arr[1];
+    };
+
+    const last = arr => {
+        return arr[arr.length-1];
+    };
+
+    console.log(first([1, 2, 3, 4, 5])); // returns 1
+    console.log(second([1, 2, 3, 4, 5])); // returns 2
+    console.log(last([1, 2, 3, 4, 5])); // return 5
 
 })();
