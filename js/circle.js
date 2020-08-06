af69ab399cd4221abe29a31f8ebcb22e0410a069
John McNay <john.w.mcnay@gmail.com>
2020-08-06 09:46:18 -0500

    finished Math object exercises

--- js/circle.js
+++ js/circle.js
@@ -9,7 +9,7 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            return; // TODO: return the proper value
+            return Math.PI * this.radius**2; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
@@ -17,8 +17,13 @@
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
+            var area = doRounding ? Math.round(this.getArea()): this.getArea();
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + area);
+        },
+
+        setRadius: function (radius) {
+            this.radius = radius
         }
     };
 
@@ -30,7 +35,7 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.setRadius(5);
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
