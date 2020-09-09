0a7c1e169d6733cfcb972fc63b9d1aeff19297a9
John McNay <john.w.mcnay@gmail.com>
2020-09-09 09:48:31 -0500

    jquery event exercise completed

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -6,11 +6,28 @@
 
 // $('.codeup').css('border', '1px solid red');
 
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
+//
+// $('h1, p, li').css('background-color', 'yellow');
+//
+//
+// $(function() {
+//     alert($('h1').html());
+// })
 
-$('h1, p, li').css('background-color', 'yellow');
-
-
-$(function() {
-    alert($('h1').html());
+$("h1").click(function(){
+    $(this).css('background-color', "gray");
 })
+
+$("p").dblclick(function(){
+    $(this).css("font-size", "18px");
+})
+
+$("li").hover(
+    function(){
+        $(this).css("color", "red")
+    },
+    function(){
+        $(this).css("color", "black")
+    }
+);
