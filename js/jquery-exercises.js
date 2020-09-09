897622623172449a70c675d4ee33207618712347
John McNay <john.w.mcnay@gmail.com>
2020-09-09 09:42:45 -0500

    jquery selectors exercise completed

--- js/jquery-exercises.js
+++ js/jquery-exercises.js
@@ -1,3 +1,16 @@
-$(document).ready(function() {
-    alert( 'jQuery: The DOM has finished loading!');
-});
+// $(document).ready(function() {
+//     alert( 'jQuery: The DOM has finished loading!');
+// });
+
+// $('#main').css('background-color', 'gray');
+
+// $('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+
+$('h1, p, li').css('background-color', 'yellow');
+
+
+$(function() {
+    alert($('h1').html());
+})
