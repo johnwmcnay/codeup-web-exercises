8848916861efb4be90f295e8e08a6cb407e01bf0
John McNay <john.w.mcnay@gmail.com>
2020-09-11 14:48:14 -0500

    completed bonus for jQuery FAQ part 2

--- js/jquery-faq.js
+++ js/jquery-faq.js
@@ -30,4 +30,42 @@ $("h3").click(function() {
 
 $("li").click(function() {
     $(this).parent().children().first().toggleClass("li-text")
-})
+});
+
+$("#left-swap").click(function() {
+    let originalLeft = $(this).prev().children("img").attr("src");
+    let originalCenter = $("#center-swap").prev().children("img").attr("src");
+
+    $(this).prev().children("img").attr("src", originalCenter);
+    $("#center-swap").prev().children("img").attr("src", originalLeft);
+
+});
+
+// The left frame swaps to the right and takes the image from the frame in the center.
+//     The center frame swaps randomly to either the left or right.
+//     The right frame swaps to the left and takes the image from the frame in the center.
+
+$("#center-swap").click(function() {
+    let originalCenter = $(this).prev().children("img").attr("src");
+    let originalLeft = $("#left-swap").prev().children("img").attr("src");
+    let originalRight = $("#right-swap").prev().children("img").attr("src");
+    let rand = Math.floor(Math.random() * 2);
+
+    if (rand) {
+        $("#left-swap").prev().children("img").attr("src", originalCenter);
+        $(this).prev().children("img").attr("src", originalLeft);
+    } else {
+        $("#right-swap").prev().children("img").attr("src", originalCenter);
+        $(this).prev().children("img").attr("src", originalRight);
+    }
+
+
+});
+
+$("#right-swap").click(function() {
+    let originalRight = $(this).prev().children("img").attr("src");
+    let originalCenter = $("#center-swap").prev().children("img").attr("src");
+
+    $(this).prev().children("img").attr("src", originalCenter);
+    $("#center-swap").prev().children("img").attr("src", originalRight);
+});
