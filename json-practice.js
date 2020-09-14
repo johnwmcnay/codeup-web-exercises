67a9c24c459bea6331eaf0efd42dcd67cb9bd79e
John McNay <john.w.mcnay@gmail.com>
2020-09-14 12:32:55 -0500

    ajax store exercise, able to add to JSON and update page without refresh

---
+++ json-practice.js
@@ -0,0 +1,24 @@
+let cohort = {
+    "name": "Bash",
+    "numOfStudents": 2,
+    "location": "Dallas, TX",
+    "students": [
+        {
+            "name": "Stacy R",
+            "dateOfBirth": "1990-01-24",
+            "scholarship": true,
+            "capstone": null,
+        },
+        {
+            "name": "Juan M",
+            "dateOfBirth": "1992-05-13",
+            "scholarship": false,
+            "capstone": null,
+        }
+    ],
+    "remote": false,
+    "instructors": [
+        "Fernando M",
+        "Douglas H"
+    ]
+}
