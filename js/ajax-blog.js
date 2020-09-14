40b3594b2c54d4a507a39df7b6fabbd99ca39dff
John McNay <john.w.mcnay@gmail.com>
2020-09-14 16:48:53 -0500

    finished ajax-blog exercise

--- js/ajax-blog.js
+++ js/ajax-blog.js
@@ -1,27 +1,36 @@
 
-(function() {
+$(function() {
     $.get("data/blog.json").done(function(data) {
-
-        let template = $("#template");
-
-        // const properties = ["title", "content", "date", "categories"];
         for (let obj of data) {
-            let element = document.createElement("div")
-            $(element).html($(template).html());
-            $(element).addClass("post container w-50 mb-3");
-            $(element).find(".date").html(obj.date);
-            $(element).find(".categories").html(
-                "tags:&nbsp;<span class='tags'>" + obj.categories.join(" ") + "</span>")
-            $(element).find(".content").html(obj.content);
-            $(element).find(".main-title").html(obj.title);
-
-            $("#posts").append(element);
-
+            addPost(obj);
         }
     });
 
+    function addPost(obj) {
+        let element = document.createElement("div")
+        let template = $("#template");
 
+        $(element).html($(template).html());
+        $(element).addClass("post container w-50 mb-3");
+        $(element).find(".date").html(obj.date);
+        $(element).find(".categories").html(
+            "tags:&nbsp;<span class='tags'>" + obj.categories.join(" ") + "</span>")
+        $(element).find(".content").html(obj.content);
+        $(element).find(".main-title").html(obj.title);
 
+        $("#posts").prepend(element);
+    }
 
+    setInterval(function(){
+        //check for new posts
+        $.get("data/blog.json").done(function(data) {
+            for (let obj of data) {
+                let page = $("#posts").html();
+                if ((page.indexOf(obj.title) === -1) && (page.indexOf(obj.content) === -1)) {
+                    addPost(obj);
+                }
+            }
+        });
 
-})();
+    }, 5000);
+});
