fc089522e82cf732b3f03e55e2c26f6ed0f5a285
John McNay <john.w.mcnay@gmail.com>
2020-09-21 11:51:37 -0500

    completed bonus promise exercise

--- js/promises.js
+++ js/promises.js
@@ -8,3 +8,14 @@ gitData("https://api.github.com/users/johnwmcnay/events")
     .then(data => {
         console.log(data[0].created_at);
     });
+
+function wait(delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(`You will see this after ${(delay / 1000).toFixed(2)} seconds.`);
+        }, delay);
+    });
+}
+
+wait(3000).then(msg => console.log(msg));
+wait(1500).then(msg => console.log(msg));
